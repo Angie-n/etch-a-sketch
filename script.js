7f47af0751646ccab6a81fbb4d1cc85faccc0ade
Angie Nguyen <angie.ta.nguyen@gmail.com>
2021-09-01 00:05:54 -0700

    Canvas can now reset with user's desired dimensions

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-// Creates default grid
 let gridLength = 16;
 let numGrids = Math.pow(gridLength, 2);
 let gridContainer = document.querySelector("#grid-container");
-for (numGrids; numGrids > 0; numGrids--) {
-    let newGridTile = document.createElement('div');
-    gridContainer.appendChild(newGridTile);
-}
-gridContainer.style.grid = `repeat(${gridLength}, 1fr) / repeat(${gridLength}, 1fr)`;
 
-//Causes grid tiles to get progressively darker the more times they are hovered over
+function createGrid() {
+    for (let i = numGrids; i > 0; i--) {
+        let newGridTile = document.createElement('div');
+        gridContainer.appendChild(newGridTile);
+    }
+    gridContainer.style.grid = `repeat(${gridLength}, 1fr) / repeat(${gridLength}, 1fr)`;
+}
+
+function addShadeFeature() {
+    gridChildren.forEach(node => {
+        node.setAttribute("data-shade", "256");
+        node.addEventListener("mouseover", shadeDarker);
+    });
+}
+
 function shadeDarker() {
     if (this.dataset.shade > 0) {
         this.setAttribute("data-shade", `${this.dataset.shade - 25.6}`);
@@ -16,8 +24,28 @@ function shadeDarker() {
     }
 }
 
+function resetGrid() {
+    gridLength = parseInt(window.prompt("Enter desired length of grid (1-100)"));
+    if (gridLength < 1 || gridLength > 100 || isNaN(gridLength)) {
+        window.alert("Grid not reset; enter a valid input to change the canvas.");
+    }
+    else {
+        numGrids = Math.pow(gridLength, 2);
+        deleteCurrentGrid();
+        createGrid();
+        gridChildren = document.querySelectorAll("#grid-container > div");
+        addShadeFeature();
+    }
+}
+
+function deleteCurrentGrid() {
+    gridChildren.forEach(node => {
+        node.remove();
+    })
+}
+
+createGrid();
 let gridChildren = document.querySelectorAll("#grid-container > div");
-gridChildren.forEach(node => {
-    node.setAttribute("data-shade", "256");
-    node.addEventListener("mouseover", shadeDarker);
-});
+addShadeFeature();
+let resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", resetGrid);
