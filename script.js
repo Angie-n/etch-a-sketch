6de9f8611742cd575d025fff93e27975132f88ae
Angie Nguyen <angie.ta.nguyen@gmail.com>
2021-08-31 22:32:55 -0700

    Tiles become darker when hovered over, 10 times max

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Creates default grid
 let gridLength = 16;
 let numGrids = Math.pow(gridLength, 2);
 let gridContainer = document.querySelector("#grid-container");
@@ -6,4 +7,17 @@ for (numGrids; numGrids > 0; numGrids--) {
     gridContainer.appendChild(newGridTile);
 }
 gridContainer.style.grid = `repeat(${gridLength}, 1fr) / repeat(${gridLength}, 1fr)`;
-console.log(gridContainer);
+
+//Causes grid tiles to get progressively darker the more times they are hovered over
+function shadeDarker() {
+    if (this.dataset.shade > 0) {
+        this.setAttribute("data-shade", `${this.dataset.shade - 25.6}`);
+        this.style.backgroundColor = `rgb(${this.dataset.shade},${this.dataset.shade},${this.dataset.shade})`;
+    }
+}
+
+let gridChildren = document.querySelectorAll("#grid-container > div");
+gridChildren.forEach(node => {
+    node.setAttribute("data-shade", "256");
+    node.addEventListener("mouseover", shadeDarker);
+});
